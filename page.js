c03abcc31ae1692f02c8666c31b5ba8609e5b598
wenqh <wen-qh@163.com>
2025-07-10 12:36:44 +0800

    Update page.js

--- page.js
+++ page.js
@@ -29,13 +29,13 @@
         if (endY - startY < -50) {
             const before = window.scrollY;
 
-            window.scrollBy(0, window.innerHeight-50);
+            window.scrollBy(0, window.innerHeight-20);
 
             requestAnimationFrame(() => {
                 _hook();
             });
         } else if (endY - startY > 50) {
-            window.scrollBy(0, -window.innerHeight+50);
+            window.scrollBy(0, -window.innerHeight+20);
         }
     });
 })();
